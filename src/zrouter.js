22a1909b436799ab8a863944b09c1339fd33a5de
wuzijie <wuzijie_1991@163.com>
2015-04-12 17:28:43 +0800

    dispatch,mount

--- src/zrouter.js
+++ src/zrouter.js
@@ -15,12 +15,54 @@
   var dloc = document.location;
 
   /**
+   * Shorthand: hasOwn
+   * stand for hasOwnProperty
+   * @param {Object} obj
+   * @param {String} p
+   * @return {Boolean}
+   */
+  var hasOwn = function(obj, p) {
+    return obj.hasOwnProperty(p);
+  };
+
+  /**
+   * Utils: Each
+   * @param {Array} arr
+   * @param {Function} iterator
+   */
+  var each = function(arr, iterator) {
+    for (var i = 0, len = arr.length; i < len; ++i) {
+      if (iterator(arr[i], i, arr) === false) {
+        return;
+      }
+    }
+  }
+
+  /**
    * Utils: isArray
    * @param {Obejct} obj
    * @return {Boolean}
    */
   var isArray = function(obj) {
     return Object.prototype.toString.call(obj) === "[object Array]";
+  };
+
+  /**
+   * Utils: isFunction
+   * @param {Object} obj
+   * @return {Boolean}
+   */
+  var isFunction = function(obj) {
+    return Object.prototype.toString.call(obj) === "[object Function]";
+  };
+
+  /**
+   * Utils: isPlainObject
+   * @param {Object} obj
+   * @return {Boolean}
+   */
+  var isPlainObject = function(obj) {
+    return Object.prototype.toString.call(obj) === "[object Object]";
   };
 
   /**
@@ -32,12 +74,42 @@
   };
 
   /**
+   * Utils: clone
+   * @param {Object} obj
+   * @return {Object}
+   */
+  var clone = function(obj) {
+    var newObj = {};
+    return newObj;
+  };
+
+  /**
    * Utils: mixin
    * @param {Object} destination
    * @param {Object} source
    * @return {Object}
    */
   var mixin = function(destination, source) {
+    var newObj = {};
+    // if (isPlainObject(destination)) {
+    //   for (var o in destination) {
+    //     if (destination.hasOwnProperty(o)) {
+
+    //     }
+    //   }
+    // } else {
+    //   destination = {};
+    // }
+    // for (var p in source) {
+    //   if (source.hasOwnProperty(p)) {
+    //     var _dest = destination[p], _src = source[p];
+    //     if (isPlainObject(_src) && isPlainObject[_dest]) {
+    //       newObj[p] = mixin(_dest, _src);
+    //     }
+    //     if ()
+    //     newObj[p] = source[p];
+    //   }
+    // }
     return {};
   };
 
@@ -104,15 +176,19 @@
   window.onhashchange = onchange;
 
   /**
-   * Constructor: Router
-   * @param {Object} routes
+   * Constructor: Router (routes)
+   * @param {Object} routes **Optional**
    */
   var Router = exports.Router = function(routes) {
     if (!(this instanceof Router)) return new Router(routes);
     // 规则化参数
     this.params = {};
-    // 路由
     this.routes = {};
+    // 挂载
+    mount.call(this, routes);
+    this.options = {};
+    // 初始化配置
+    this.configure();
   };
 
   /**
@@ -120,12 +196,14 @@
    * @return this
    */
   Router.prototype.init = function(root) {
+    var self = this;
     // 一个Router实例对应一个listener，并按照初始化顺序添加到Router.listeners数组中
     // handler单独处理该路由实例的所有路由
     this.handler = function(onChangeEvent) {
       var newURL = onChangeEvent && onChangeEvent.newURL || window.location.hash;
       var url = newURL.replace(/.*#/, '');
-      dispatch('on', url.charAt(0) === '/' ? url : '/' + url);
+      // dispatch
+      dispatch.call(self, url.charAt(0) === '/' ? url : '/' + url);
     };
 
     Listener.add(this.handler);
@@ -133,26 +211,23 @@
 
   /**
    * @param {String|RegExp} path
-   * @param {Function|Array} handle
+   * @param {Function|Array} handler
    * @return this
    */
-  Router.prototype.on = Router.prototype.route = function(path, handle) {};
+  Router.prototype.on = Router.prototype.route = function(path, handler) {};
 
   /**
-   * Default Configuration
-   * {Object}
-   */
-  var defaults = {};
-
-  /**
-   * @param {Object} options
+   * @param {Object} options **Optional**
    * @return this
    */
-  Router.prototype.configure = function(options) {};
+  Router.prototype.configure = function(options) {
+    options = options || {};
+    this.notfound = options.notFound;
+  };
 
   /**
    * @param {String|Array} token
-   * @param {String|RegExp} pattern
+   * @param {String|RegExp} matcher
    * @return this
    */
   Router.prototype.param = function(token, matcher) {
@@ -163,6 +238,33 @@
     return this;
   };
 
-  function dispatch() {}
+  function mount(routes) {
+    for (var p in routes) {
+      if (hasOwn(routes, p)) {
+        if (isFunction(routes[p])) {
+          var rule = new RegExp('^' + p + '$');
+          this.routes[rule.source] = routes[p];
+        }
+      }
+    }
+  }
+
+  function dispatch(path) {
+    var routes = this.routes;
+    var match = false;
+    for (var p in routes) {
+      if (hasOwn(routes, p)) {
+        var pathRegExp = new RegExp(p);
+        if(pathRegExp.test(path)) {
+          match = true;
+          routes[p].call(this);
+          break;
+        }
+      }
+    }
+    if (!match && this.notfound) {
+      this.notfound.call(this);
+    }
+  }
 
 }));
